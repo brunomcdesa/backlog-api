0846247d9a81572861fc732d95353970b10397eb
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2025-01-12 10:53:22 -0300

    Initial commit

---
+++ src/main/java/br/com/backolog/backlogapi/BacklogApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.backolog.backlogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacklogApiApplication.class, args);
+    }
+
+}
